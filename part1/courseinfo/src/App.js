b9fbfdca0d3816c65516d21259632ce228813be1
Sonlis <bastien.jeannelle@gmail.com>
2022-10-23 22:02:12 +0300

    exercice 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -10,7 +10,9 @@ const App = () => {
   return (
         <div>
             <Header course={course} />
-            <Content part1={part1} exercices1={exercises1} part2={part2} exercices2={exercises2} part3={part3} exercices3={exercises3} />
+            <Content part={part1} exercices={exercises1}  />
+            <Content part={part2} exercices={exercises2}  />
+            <Content part={part3} exercices={exercises3}  />
             <Total exercices1={exercises1} exercices2={exercises2} exercices3={exercises3} />
         </div>
   )
@@ -27,13 +29,7 @@ const Content = (props) => {
     return (
         <>
             <p>
-                {props.part1} {props.exercices1}
-            </p>
-            <p>
-                {props.part2} {props.exercices2}
-            </p>
-            <p>
-                {props.part3} {props.exercices3}
+                {props.part} {props.exercices}
             </p>
         </>
     )
