26f2d153ff9a1af3f664168f6550decf890c3c0c
Sonlis <bastien.jeannelle@gmail.com>
2022-10-23 22:11:49 +0300

    exercice 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,25 @@
 const App = () => {
-  const course = 'Half Stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
+    const course = 'Half Stack application development'
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
 
   return (
         <div>
             <Header course={course} />
-            <Content part={part1} />
-            <Content part={part2} />
-            <Content part={part3} />
-            <Total exercices1={part1.exercises} exercices2={part2.exercises} exercices3={part3.exercises} />
+            <Content parts={parts} />
+            <Total parts={parts} />
         </div>
   )
 }
@@ -35,7 +35,13 @@ const Content = (props) => {
     return (
         <>
             <p>
-                {props.part.name} {props.part.exercises}
+                {props.parts[0].name} {props.parts[0].exercises}
+            </p>
+            <p>
+                {props.parts[1].name} {props.parts[1].exercises}
+            </p>
+            <p>
+                {props.parts[2].name} {props.parts[2].exercises}
             </p>
         </>
     )
@@ -44,7 +50,7 @@ const Content = (props) => {
 
 const Total = (props) => {
     return(
-        <p>Number of exercises {props.exercices1 + props.exercices2 + props.exercices3}</p>
+        <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     )
 
 }
