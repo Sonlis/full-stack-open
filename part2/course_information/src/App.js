8d8c11652c375c3823beec8aa4690a403211b738
Sonlis <bastien.jeannelle@gmail.com>
2022-11-04 16:21:35 +0200

    exercise 2.4

--- part2/course_information/src/App.js
+++ part2/course_information/src/App.js
@@ -28,36 +28,68 @@ const Content = ({ course }) => {
     )
 }
 
-const Course = ({course}) =>
+const Course = ({courses}) =>
     <>
-      <Header course={course}></Header>
-      <Content course={course}></Content>
+        {courses.map(course => {
+            return (
+                <>
+                    <Header key={course.id} course={course}></Header>
+                    <Content key={course.id }course={course}></Content>
+                </>
+            )
+        }
+
+        )}
     </>
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
     ]
-  }
 
-  return <Course course={course} />
+
+  return <Course courses={courses} />
 }
 
 export default App
