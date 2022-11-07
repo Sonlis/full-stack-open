fd8b7aad1b3e7de534dec9bce114ec5b5217676a
Sonlis <bastien.jeannelle@gmail.com>
2022-11-07 23:34:04 +0200

    Exercise 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,42 @@
 import { useState } from 'react'
 
+const Filter = (props) => {
+    return (
+        <>
+            <h2>Filter</h2>
+            <input onChange={props.filterNames} />
+        </>
+    )
+}
+
+const PersonForm = (props) => {
+    return (
+        <form onSubmit={props.setNewPerson}>
+            <div>
+                name: <input value={props.newName} onChange={props.handleNameChange}/>
+            </div>
+            <div>number: <input value={props.newNumber} onChange={props.handleNumberChange}/></div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+
+        </form>
+    )
+}
+
+const Persons = (props) => {
+    return (
+        <>
+            {props.persons.map((person) => {
+                if (person.name.includes(props.filter)) {
+                    return (
+                        <p key={person.name}>{person.name} {person.number}</p>
+                    )
+                }})}
+        </>
+    )
+}
+
 const App = () => {
     const [persons, setPersons] = useState([
         { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -7,13 +44,13 @@ const App = () => {
         { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
         { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ])
+    const [filter, setNewFilter] = useState('');
+    const filterNames = (event) => setNewFilter(event.target.value);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
-    const [filter, setNewFilter] = useState('')
+
     const handleNameChange = (event) => setNewName(event.target.value);
     const handleNumberChange = (event) => setNewNumber(event.target.value);
-
-    const filterNames = (event) => setNewFilter(event.target.value);
 
     const setNewPerson = (event) => {
         event.preventDefault();
@@ -33,27 +70,11 @@ const App = () => {
     return (
       <div>
         <h2>Phonebook</h2>
-          <input onChange={filterNames} />
-        <form onSubmit={setNewPerson}>
-          <div>
-              <h2>add a new</h2>
-            name: <input value={newName} onChange={handleNameChange}/>
-          </div>
-            <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
-          <div>
-            <button type="submit">add</button>
-          </div>
-
-        </form>
+          <Filter filterNames={filterNames}/>
+        <h2>Add a new</h2>
+          <PersonForm newName={newName} newNumber={newNumber} setNewPerson={setNewPerson} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
         <h2>Numbers</h2>
-        <>
-            {persons.map((person) => {
-                if (person.name.includes(filter)) {
-                    return (
-                        <p key={person.name}>{person.name} {person.number}</p>
-                    )
-                }})}
-        </>
+          <Persons persons={persons} filter={filter} />
       </div>
     )
 }
