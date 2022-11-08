bb86e412cf9695118441ad2a2a6d0fac307fae31
Sonlis <bastien.jeannelle@gmail.com>
2022-11-08 09:19:15 +0200

    exercises 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => {
     return (
@@ -38,12 +39,18 @@ const Persons = (props) => {
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const hook = () => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }
+
+    useEffect(hook, [])
+    const [persons, setPersons] = useState([]);
     const [filter, setNewFilter] = useState('');
     const filterNames = (event) => setNewFilter(event.target.value);
     const [newName, setNewName] = useState('');
