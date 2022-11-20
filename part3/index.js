e1c09e37acc3663557eb765dca0b4b3ef1df72be
Sonlis <bastien.jeannelle@gmail.com>
2022-11-20 17:54:56 +0200

    Exercises 3.6

--- part3/index.js
+++ part3/index.js
@@ -56,6 +56,16 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body;
+    if (body.name === undefined || body.number === undefined) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+    if (data.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'Name already exists'
+        })
+    }
     const person = {
         id: Math.random() * 1000,
         name: body.name,
